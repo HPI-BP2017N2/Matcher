5a19056c8caad02a3ba4d0967160ab0233121008
dey4ss <lindnerdaniel@yahoo.de>
2018-04-09 17:30:43 +0200

    initial commit

---
+++ src/main/java/de/hpi/matcher/MatcherApplication.java
@@ -0,0 +1,12 @@
+package de.hpi.matcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatcherApplication.class, args);
+    }
+}
