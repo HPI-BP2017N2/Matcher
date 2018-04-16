c22d0e4ab2a16dfd9dab34ca5c325b41ead717b2
dey4ss <lindnerdaniel@yahoo.de>
2018-04-16 18:18:58 +0200

    connect to cache #1

---
+++ src/main/java/de/hpi/matcher/dto/ShopOffer.java
@@ -0,0 +1,30 @@
+package de.hpi.matcher.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Map;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class ShopOffer {
+
+    private String offerKey;
+    private Long shopId;
+    private String brandName;
+    private List<String> categoryPaths;
+    private String productSearchtext;
+    private String ean;
+    private String han;
+    private String sku;
+    private Map<String, String> titles;
+    private Map<String, Double> prices;
+    private Map<String, String> descriptions;
+    private Map<String, String> urls;
+    private List<String> hans;
+    private List<String> eans;
+    private byte phase;
+}
---
+++ src/main/java/de/hpi/matcher/services/Cache.java
@@ -0,0 +1,81 @@
+package de.hpi.matcher.services;
+
+import de.hpi.matcher.dto.ShopOffer;
+import de.hpi.matcher.properties.CacheProperties;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Getter(AccessLevel.PRIVATE)
+@Setter(AccessLevel.PRIVATE)
+@Repository
+public class Cache {
+
+    private RestTemplate restTemplate;
+
+    private CacheProperties properties;
+
+    @Autowired
+    public Cache(RestTemplateBuilder restTemplateBuilder, CacheProperties cacheProperties) {
+        setProperties(cacheProperties);
+        setRestTemplate(restTemplateBuilder.build());
+    }
+
+    public void warmup(long shopId) {
+        getRestTemplate().getForObject(warmupURI(shopId), Object.class);
+    }
+
+    public ShopOffer getOffer(long shopId, byte phase) {
+        return getRestTemplate().getForObject(getOffersURI(shopId, phase), ShopOffer.class);
+    }
+
+    public void deleteOffer(long shopId, String offerKey) {
+        getRestTemplate().delete(deleteOfferURI(shopId, offerKey));
+    }
+
+    public void deleteAll(long shopId) {
+        getRestTemplate().delete(deleteAllURI(shopId));
+    }
+
+    private URI warmupURI(long shopID) {
+        return UriComponentsBuilder.fromUriString(getProperties().getUri())
+                .path(getProperties().getWarmupRoute() + shopID)
+                .build()
+                .encode()
+                .toUri();
+    }
+
+    private URI getOffersURI(long shopID, byte phase) {
+        return UriComponentsBuilder.fromUriString(getProperties().getUri())
+                .path(getProperties().getGetOfferRoute() + shopID)
+                .queryParam("phase", phase)
+                .build()
+                .encode()
+                .toUri();
+    }
+
+    private URI deleteOfferURI(long shopID, String offerKey) {
+        return UriComponentsBuilder.fromUriString(getProperties().getUri())
+                .path(getProperties().getDeleteOfferRoute() + shopID)
+                .queryParam("offerKey", offerKey)
+                .build()
+                .encode()
+                .toUri();
+    }
+
+    private URI deleteAllURI(long shopID) {
+        return UriComponentsBuilder.fromUriString(getProperties().getUri())
+                .path(getProperties().getDeleteAllRoute() + shopID)
+                .build()
+                .encode()
+                .toUri();
+    }
+
+}
