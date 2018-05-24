c08f4269f6919a675f991b1b509b86ed62b354f8
Daniel Lindner <lindnerdaniel@yahoo.de>
2018-05-24 23:08:20 +0200

    [WIP] fix test #2

--- src/test/java/de/hpi/matcher/services/MatcherServiceTest.java
+++ src/test/java/de/hpi/matcher/services/MatcherServiceTest.java
@@ -56,7 +56,10 @@ public class MatcherServiceTest {
                 getCache(),
                 null,
                 getParsedOfferRepository(),
-                getMatchingResultRepository()
+                getMatchingResultRepository(),
+                null,
+                null,
+                null
         ));
 
     }
